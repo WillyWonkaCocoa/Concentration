7c15ff1ce50b838aa9e6838bf35add50b11116a5
William Gao <wgao1@umbc.edu>
2018-04-17 21:56:54 -0400

    Add dog emoji card

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by William Gao on 4/17/18.
+//  Copyright © 2018 William Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //methods and instance variables
+    
+    //method called when dog button is pressed
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🐶", on: sender)
+    }
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🙊", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9220425086, green: 0.3960026836, blue: 1, alpha: 1);
+        }else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1);
+        }
+    }
+}
+
